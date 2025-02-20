441971939f50e0def77f088c05698e1731200fb4
Archana-97104 <156064407+Archana-97104@users.noreply.github.com>
2025-02-20 22:10:38 +0530

    Create Excel.java

---
+++ Excel.java
@@ -0,0 +1,61 @@
+package GrafanaSingleCase;
+
+
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.testng.annotations.DataProvider;
+
+public class Excel {
+
+    @DataProvider(name = "DropdownData")
+    public Object[][] getData() throws IOException {
+        String filePath = "./data/Input.xlsx";
+        FileInputStream fileInputStream = new FileInputStream(filePath);
+        XSSFWorkbook workbook = new XSSFWorkbook(fileInputStream);
+        XSSFSheet sheet = workbook.getSheetAt(0);
+
+        List<Object[]> data = new ArrayList<>();
+        Iterator<Row> rows = sheet.iterator();
+
+        // Skip header row if present
+        if (rows.hasNext()) rows.next();
+
+        // Iterate through each row and read cell data
+        while (rows.hasNext()) {
+            Row row = rows.next();
+            int cellCount = row.getLastCellNum(); 
+            String[] rowData = new String[cellCount];
+
+            for (int i = 0; i < cellCount; i++) {
+                Cell cell = row.getCell(i);
+                rowData[i] = (cell != null) ? cell.toString() : "";
+            }
+
+            // Ensure that rowData has at least 6 columns (handling missing values)
+            if (rowData.length < 6) {
+                String[] extendedRowData = new String[6];
+                System.arraycopy(rowData, 0, extendedRowData, 0, rowData.length);
+                for (int i = rowData.length; i < 6; i++) {
+                    extendedRowData[i] = ""; // Assign empty string if value is missing
+                }
+                data.add(extendedRowData);
+            } else {
+                data.add(rowData);
+            }
+        }
+
+        workbook.close();
+        return data.toArray(new Object[0][0]);
+    }
+
+  
+}
